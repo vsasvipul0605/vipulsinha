f54625d64afdf543d7d23c8238e25890cd79d42a
vsasvipul0605 <vsasvipul@gmail.com>
2021-01-29 12:09:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import MyNavbar from "./components/my-navbar/my-navbar.component";
-import MyCarousal from "./components/my-carousal/my-carousal.component";
-import MyTitleMessage from "./components/title-message/title-message.component";
-import About from "./pages/about/about.component";
-import Skills from "./pages/skills/skills.component";
+import MyNavbar from "./components/my-navbar/my-navbar.component.jsx";
+import MyCarousal from "./components/my-carousal/my-carousal.component.jsx";
+import MyTitleMessage from "./components/title-message/title-message.component.jsx";
+import About from "./pages/about/about.component.jsx";
+import Skills from "./pages/skills/skills.component.jsx";
 import Container from "react-bootstrap/Container";
-import TimeLine from "./components/projects-timeline/projects-timeline.component";
-import Experience from "./pages/experience/experience.component";
+import TimeLine from "./components/projects-timeline/projects-timeline.component.jsx";
+import Experience from "./pages/experience/experience.component.jsx";
 import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
-import ContactForm from "./pages/contact-form/contact-form.component";
+import ContactForm from "./pages/contact-form/contact-form.component.jsx";
 import { Parallax } from "react-parallax";
 // import Particles from "react-particles-js";
 // import { particlesOptions } from "./particlesOptions";
-import FooterPanel from "./components/footer/footer.component";
+import FooterPanel from "./components/footer/footer.component.jsx";
 
 import "./App.css";
 
